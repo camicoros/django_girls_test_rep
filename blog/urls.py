54ba8b8e3b4eb0aadebf67e1174fe2e0fcd2368a
Denis <d.saushkin@directline.digital>
2021-10-14 15:31:58 +0400

    Changed the HTML for the site

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 
+from . import views
 
 
 urlpatterns = [
-    path('', login_required(TemplateView.as_view(template_name="index.html", extra_context={'text':"Here's the text of the Web page.", }), login_url='/admin/login/'), name='index'),
+    path('', views.post_list, name='post_list'),
     path('zazu', login_required(TemplateView.as_view(template_name="index.html", extra_context={'text':"Here's the text of the Web page.", }), login_url='/admin/login/'), name='zazu'),
 ]
